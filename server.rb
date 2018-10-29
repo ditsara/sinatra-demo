3f650007c5196427a760d54b6a22e66d3be1412c
Dan Itsara <dan@glazziq.com>
2018-10-29 10:37:15 +0700

    basic usage

--- server.rb
+++ server.rb
@@ -3,5 +3,17 @@
 require 'sinatra'
 
 get '/' do
-  "ok"
+  "Hello World!"
 end
+
+# configs
+
+# TL;DR - next line is required only if you're running docker.
+#
+# By default, Sinatra binds only to 'localhost', meaning the development web
+# server will only respond to requests from the local machine. When running in
+# a docker container we need to bind to '0.0.0.0' so the server will listen to
+# all available interfaces, which allows us to access it from outside the
+# docker development container.
+set :bind, '0.0.0.0'
+
